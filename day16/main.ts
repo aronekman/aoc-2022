dad2f5e2ab8e39ecdc6af89e6ce4db4fc16477cf
aronekman <aron99ekman@gmail.com>
2022-12-17 23:45:07 +0200

    day16 part1

--- day16/main.ts
+++ day16/main.ts
@@ -3,42 +3,66 @@ import { readFileSync } from 'fs';
 interface Valve {
   rate: number;
   leadsTo: string[];
-
   open: boolean;
-
 }
 
 const runTime = 20;
 const main = () => {
   const input = readFileSync('day16/input.txt', 'utf-8');
   const valves: Map<string, Valve> = new Map(
-    input.split('\n').map(row => {
-      return [row.match(/Valve \S\S/)[0].split(' ')[1],
+    input.split('\n').map((row) => {
+      return [
+        row.match(/Valve \S\S/)[0].split(' ')[1],
         {
           rate: Number(row.match(/rate=\d+/)[0].split('=')[1]),
           leadsTo: row.split(';')[1].match(/[A-Z][A-Z]/g),
           open: false
-        }];
+        }
+      ];
     })
   );
 
-  const startTime = Date.now();
-  console.log(part1(new Map(valves), 'AA', 1, 0));
-  console.log(((Date.now() - startTime) / 1000).toFixed(2));
+  console.log(part1(new Map(valves)));
 };
 
-const part1 = (valves: Map<string, Valve>, current: string, minute: number, pressure: number): number => {
-  if (minute === runTime) return pressure;
-  if (![...valves].some(([_, v]) => v.rate > 0 && !v.open)) {
-    return (runTime - minute) * pressure;
+const part1 = (valves: Map<string, Valve>) => {
+  const bestRoutes: Map<string, { sum: number; perMinute: number; opened: string[] }> = new Map([
+    ['AA', { sum: 0, perMinute: 0, opened: [] }]
+  ]);
+
+  let i = 1;
+  while (i < 30) {
+    const prevBest: Map<string, { sum: number; perMinute: number; opened: string[] }> = new Map([...bestRoutes]);
+    bestRoutes.clear();
+    for (let [key, value] of prevBest.entries()) {
+      if (!value.opened.includes(key) && valves.get(key).rate > 0) {
+        bestRoutes.set(key, {
+          sum: value.sum + value.perMinute + valves.get(key).rate,
+          perMinute: value.perMinute + valves.get(key).rate,
+          opened: [...value.opened, key]
+        });
+      }
+    }
+    for (let [key, value] of prevBest.entries()) {
+      valves.get(key).leadsTo.forEach((v) => {
+        if (!bestRoutes.has(v) || bestRoutes.get(v).sum < value.sum + value.perMinute) {
+          bestRoutes.set(v, { sum: value.sum + value.perMinute, perMinute: value.perMinute, opened: value.opened });
+        }
+      });
+    }
+    for (let [key, value] of prevBest.entries()) {
+      if (!bestRoutes.has(key) || bestRoutes.get(key).sum < value.sum + value.perMinute) {
+        bestRoutes.set(key, { sum: value.sum + value.perMinute, perMinute: value.perMinute, opened: value.opened });
+      }
+    }
+
+    i++;
+    console.log(`== Minute ${i} ==`);
+    for (let [key, value] of bestRoutes.entries()) {
+      console.log({ key, sum: value.sum, perminute: value.perMinute, opened: value.opened.join(' ') });
+    }
   }
-  let max = pressure;
-  const currentValve: Valve = valves.get(current);
-  if (!currentValve.open && currentValve.rate > 0) {
-    valves.set(current, { ...currentValve, open: true });
-    max = Math.max(max, pressure + part1(new Map(valves), current, minute + 1, pressure + currentValve.rate));
-  }
-  return currentValve.leadsTo.reduce((a: number, b: string) => Math.max(a, pressure + part1(new Map(valves), b, minute + 1, pressure)), max);
+  return [...bestRoutes.values()].reduce((a, { sum }) => Math.max(a, sum), 0);
 };
 
 main();
